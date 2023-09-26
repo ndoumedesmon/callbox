88aa5a014bc678f13925b03d402258510327ce58
ndoumedesmon <ndoumedesmon@yahoo.com>
2023-09-26 11:55:47 +0100

    recent updates

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CallBox</title>
+</head>
+<body>
+    <h1>Bienvenu dans le site CallBox !</h1>
+    <p>Ceci est la page d'accueil !</p>
+</body>
+</html>
